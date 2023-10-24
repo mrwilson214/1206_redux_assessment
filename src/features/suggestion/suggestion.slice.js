27526bc195f1a25442dad5ebce9fdba0621e8543
Matt Wilson <wilsonm@indeed.com>
2023-10-24 15:38:15 -0700

    completed all tasks

--- src/features/suggestion/suggestion.slice.js
+++ src/features/suggestion/suggestion.slice.js
@@ -1,28 +1,43 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchSuggestion =
-  createAsyncThunk(/* Task 15: Complete the `createAsyncThunk()` function to load a suggestion from this URL: http://localhost:3004/api/suggestion */);
+export const fetchSuggestion = createAsyncThunk(
+  "suggestion/fetchSuggestion",
+  async (thunkAPI) => {
+    const res = await fetch("http://localhost:3004/api/suggestion").then(
+      (data) => data.json()
+    );
+    return res.data;
+  }
+);
 
 const initialState = {
-  suggestion: '',
+  suggestion: "",
   loading: false,
   error: true,
 };
 
 const options = {
-  name: 'suggestion',
+  name: "suggestion",
   initialState,
   reducers: {},
   extraReducers: {
-    /* Task 16: Inside `extraReducers`, add reducers to handle all three promise lifecycle states - pending, fulfilled, and rejected - for the `fetchSuggestion()` call */
+    [fetchSuggestion.pending]: (state) => {
+      state.loading = true;
+    },
+    [fetchSuggestion.fulfilled]: (state, { payload }) => {
+      state.loading = false;
+      state.suggestion = payload;
+      state.error = false;
+    },
+    [fetchSuggestion.rejected]: (state) => {
+      state.loading = false;
+    },
   },
 };
 
 const suggestionSlice = createSlice(options);
 
 export default suggestionSlice.reducer;
-
-// Task 17: Create a selector, called `selectSuggestion`, for the `suggestion` state variable and export it from the file
-
+export const selectSuggestion = (state) => state.suggestion.suggestion;
 export const selectLoading = (state) => state.suggestion.loading;
 export const selectError = (state) => state.suggestion.error;
